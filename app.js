2aca5691c06cffbc3448d5f3957b291fdf364b5f
ClarkMsan10 <clarkmassanga@gmail.com>
2023-10-24 07:42:07 +0100

    animation

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ function slider(){
 }
 
 
-  window.addEventListener("scroll", function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 10 );
+  // window.addEventListener("scroll", function(){
+  //   var header = this.document.querySelector("header");
+  //   header.classList.toggle("sticky", window.scrollY > 10 );
     
-  })
+  // })
  
 hamburger = document.querySelector(".hamburger");
 navBar = document.querySelector('.nav');
@@ -40,4 +40,39 @@ function cancelMenu(){
     navBar.classList.remove("active");
     var h = document.querySelector("header");
     h.classList.remove('bg-nav');
+}
+
+var lastScrollTop = 0;
+header1 = document.querySelector("header");
+window.addEventListener('scroll', function(){
+  var scrollTop = this.window.scrollY || this.document.documentElement.scrollTop;
+
+  if(scrollTop > lastScrollTop){
+    header1.style.top = "-120px";
+  }else{
+    header1.style.top = "0"
+  }
+  lastScrollTop = scrollTop;
+  header1.classList.toggle("sticky", window.scrollY > 10 );
+})
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('section.show');
+  console.log(reveals.length)
+  for (var i = 0; i < reveals.length; i++){
+    console.log(reveals[i].classList);
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 75;
+    // reveals[i].classList.add('active')
+    
+    if(revealtop < windowheight - revealpoint){
+    reveals[i].classList.add('active');
+    }
+    else{
+    reveals[i].classList.remove('active');
+    }
+  }
 }
